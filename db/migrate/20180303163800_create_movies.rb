dd67e4972e7b9386a382857734697d9c3f51f2eb
Dave Asay <dvasay@gmail.com>
2018-03-03 13:38:11 -0700

    adds playlist model

--- db/migrate/20180303163800_create_movies.rb
+++ db/migrate/20180303163800_create_movies.rb
@@ -6,6 +6,7 @@ class CreateMovies < ActiveRecord::Migration[5.1]
       t.string :genre
       t.string :description
       t.string :trailer
+      t.belongs_to :playlist, optional: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303164016) do
+ActiveRecord::Schema.define(version: 20180303195012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,18 @@ ActiveRecord::Schema.define(version: 20180303164016) do
     t.string "genre"
     t.string "description"
     t.string "trailer"
+    t.bigint "playlist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["playlist_id"], name: "index_movies_on_playlist_id"
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_playlists_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +64,5 @@ ActiveRecord::Schema.define(version: 20180303164016) do
 
   add_foreign_key "comments", "movies"
   add_foreign_key "comments", "users"
+  add_foreign_key "playlists", "users"
 end
