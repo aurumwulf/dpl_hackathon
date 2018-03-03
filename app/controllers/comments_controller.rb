347323972ad9035899641b6f5b2b5fee5fec0127
Tyson Au <au.tyson@gmail.com>
2018-03-03 14:20:23 -0700

    crud actions for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
-  before_action :set_movie 
-  before_action :comment_params, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   def index
     #TODO user_profile
@@ -42,7 +42,7 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body, :movie_id, :user_id)
     end
 
     def set_comment
@@ -50,6 +50,6 @@ class CommentsController < ApplicationController
     end
 
     def set_movie
-      @movie = Movie.find(params[:id])
+      @movie = Movie.find(params[:movie_id])
     end
 end
