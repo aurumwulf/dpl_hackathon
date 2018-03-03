00c0585c931bcc6cfb986d86a5df0ff66b4ea8c1
Devin Parkinson <dparky13@icloud.com>
2018-03-03 15:37:08 -0700

    add, edit, remove movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,54 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+
   def show_genre
     @genre = Movie.where(genre: params[:genre])
   end
 
   def show
-    @movie = Movie.find(params[:id])
   end
 
   def index
     @movies = Movie.all
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movie_path(@movie)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to '/'
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :duration, :genre, :description, :trailer)
   end
 end
