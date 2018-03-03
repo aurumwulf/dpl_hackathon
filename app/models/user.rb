b43e6ffda56f350fd59cf25b2d65aaa4e47184b0
Dave Asay <dvasay@gmail.com>
2018-03-03 10:49:12 -0700

    foo

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+<<<<<<< HEAD
   
+=======
+>>>>>>> foo
   has_many :comments
 end
